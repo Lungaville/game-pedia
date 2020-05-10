98d62edf7e2e0e292ba69e9000cc39e0bf62ba0f
Lungaville <lordfrozt104@gmail.com>
2020-05-11 03:34:18 +0700

    CRUD Users

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,102 @@
 var express = require('express');
 var router = express.Router();
 var httpCode = require('../utils/http-code')
+const model = require('../models/index');
+
 const { check, validationResult } = require('express-validator');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const users = await model.users.findAll({});
+  return res.json({
+    'status': 'OK',
+    'messages': '',
+    'data': users
+  })
 });
 
 router.post('/',[
-  check('username').isAlpha(),
-  check('password').isLength({min : 5 , max : 8}),
-],function(req, res, next) {
+  check('name').isAlpha(),
+  check('email').isLength({min : 5 , max : 8}),
+  check('gender').isNumeric(),
+  check('phoneNumber').isNumeric(),
+],async function(req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(httpCode.VALIDATION_FAIL).json({ errors: errors.array() });
   }
-  res.send('respond with a resource');
+  try {
+  const users = await model.users.create(req.body);
+  if (users) {
+    res.status(201).json({
+      'status': 'OK',
+      'messages': 'User berhasil ditambahkan',
+      'data': users,
+    })
+  }
+ } catch (err) {
+   res.status(400).json({
+     'status': 'ERROR',
+     'messages': err.message,
+     'data': {},
+   })
+ }
+});
+
+router.patch('/:id', async function (req, res, next) {
+  try {
+    const usersId = req.params.id;
+    const {
+      name,
+      email,
+      gender,
+      phoneNumber
+    } = req.body;
+    const users = await model.users.update({
+      name,
+      email,
+      gender,
+      phone_number: phoneNumber
+    }, {
+      where: {
+        id: usersId
+      }
+    });
+    if (users) {
+      res.json({
+        'status': 'OK',
+        'messages': 'User berhasil diupdate',
+        'data': users,
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      'status': 'ERROR',
+      'messages': err.message,
+      'data': {},
+    })
+  }
+});
+
+router.delete('/:id', async function (req, res, next) {
+  try {
+    const usersId = req.params.id;
+    const users = await model.users.destroy({ where: {
+      id: usersId
+    }})
+    if (users) {
+      res.json({
+        'status': 'OK',
+        'messages': 'User berhasil dihapus',
+        'data': users,
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      'status': 'ERROR',
+      'messages': err.message,
+      'data': {},
+    })
+  }
 });
 module.exports = router;
