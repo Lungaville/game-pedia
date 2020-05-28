09a9951b086f7bd21f665ea7d21c7bf34b5c6da3
Lungaville <lordfrozt104@gmail.com>
2020-05-28 17:28:55 +0700

    Update user game testing

--- tests/user_game_test.js
+++ tests/user_game_test.js
@@ -12,7 +12,7 @@ let idUserPro;
 let tokenAdmin;
 let idUserAdmin;
 
-describe('Users Reviews Test', function () {
+describe('Users Games Test', function () {
     before(function() {
         id_game =  global.seed_id_game;
         tokenBasic = global.tokenBasic;
@@ -21,12 +21,28 @@ describe('Users Reviews Test', function () {
         tokenAdmin = global.tokenAdmin;
         idUserAdmin = global.userAdmin.id;
     });
-    it('Basic User can insert user game', function (done) {
+    it('Cannot insert completed time if game not completed', function (done) {
         request(app)
             .post(`/users/${idUserBasic}/game`)
             .send({
                 id_game: id_game,
                 id_user: idUserBasic,
+                tipe : 1,
+                completed_time :"1"
+            })
+            .set('Accept', 'application/json')
+            .set('token', tokenBasic)
+            .expect('Content-Type', /json/)
+            .expect(responseCode.BAD_REQUEST)
+            .end(done);
+    });
+    it('Basic User can insert user game', function (done) {
+        request(app)
+            .post(`/users/${idUserBasic}/game`)
+            .send({
+                id_game: id_game,
+                tipe : 2,
+                completed_time :"1"
             })
             .set('Accept', 'application/json')
             .set('token', tokenBasic)
@@ -36,6 +52,8 @@ describe('Users Reviews Test', function () {
                 expect(response.body.message).to.equal('Successfuly inserted user game', 'Response message tidak sesuai')
                 expect(response.body.data.id_game).to.equal(id_game, 'ID Game yang diberikan pada response tidak sesuai')
                 expect(response.body.data.id_user).to.equal(idUserBasic, 'ID User yang diberikan pada response tidak sesuai')
+                expect(response.body.data.complete_time).to.equal("00:00:01","Completed Time tidak sesuai")
+                expect(response.body.data.tipe).to.equal(2,"Tipe tidak sesuai")
             })
             .end(done);
     });
@@ -44,7 +62,8 @@ describe('Users Reviews Test', function () {
             .post(`/users/${idUserPro}/game`)
             .send({
                 id_game: id_game,
-                id_user: idUserPro,
+                tipe : 2,
+                completed_time :"1"
             })
             .set('Accept', 'application/json')
             .set('token', tokenAdmin)
@@ -54,6 +73,8 @@ describe('Users Reviews Test', function () {
                 expect(response.body.message).to.equal('Successfuly inserted user game', 'Response message tidak sesuai')
                 expect(response.body.data.id_game).to.equal(id_game, 'ID Game yang diberikan pada response tidak sesuai')
                 expect(response.body.data.id_user).to.equal(idUserPro, 'ID User yang diberikan pada response tidak sesuai')
+                expect(response.body.data.complete_time).to.equal("00:00:01","Completed Time tidak sesuai")
+                expect(response.body.data.tipe).to.equal(2,"Tipe tidak sesuai")
             })
             .end(done);
     });
@@ -71,6 +92,51 @@ describe('Users Reviews Test', function () {
           .end(done);
     });
     
+    it('Basic User can edit user game', function (done) {
+        request(app)
+            .patch(`/users/${idUserBasic}/game/${id_game}`)
+            .send({
+                tipe : 3,
+                completed_time :"12:12:12"
+            })
+            .set('Accept', 'application/json')
+            .set('token', tokenBasic)
+            .expect('Content-Type', /json/)
+            .expect(responseCode.OK)
+            .end(done);
+    });
+    it('GET all user games data should be updated', function (done) {
+      request(app)
+          .get(`/users/${idUserBasic}/game`)
+          .set('token', tokenBasic)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(responseCode.OK)
+          .expect(response => {
+            let data = response.body.data.find((item) => item.id_game == id_game);
+            expect(data.id_game).to.equal(id_game,'ID game sesuai')
+            expect(data.tipe).to.equal(3,'Tipe tidak sesuai dengan yang diupdate')
+            expect(data.complete_time).to.equal("12:12:12",'Complete time tidak sesuai dengan yang di update')
+          })
+          .end(done);
+    });
+    
+    it('GET all user games by game_id data should be updated', function (done) {
+        request(app)
+            .get(`/users/game/${id_game}`)
+            .set('token', tokenBasic)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(responseCode.OK)
+            .expect(response => {
+              let data = response.body.data.find((item) => item.id_user == idUserBasic);
+              expect(data.id_game).to.equal(id_game,'ID game sesuai')
+              expect(data.tipe).to.equal(3,'Tipe tidak sesuai dengan yang diupdate')
+              expect(data.complete_time).to.equal("12:12:12",'Complete time tidak sesuai dengan yang di update')
+            })
+            .end(done);
+      });
+      
     it('Basic User can delete game', function (done) {
         request(app)
             .delete(`/users/${idUserBasic}/game/${id_game}`)
