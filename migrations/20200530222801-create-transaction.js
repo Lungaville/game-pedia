f9b2f63028f4b0367b8c269e9b04e3e87377f45f
Lungaville <lordfrozt104@gmail.com>
2020-05-31 07:29:11 +0700

    Rename table name

--- migrations/20200530222801-create-transaction.js
+++ migrations/20200530222801-create-transaction.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Transactions', {
+    return queryInterface.createTable('transactions', {
       id: {
         allowNull: false,
         autoIncrement: true,
